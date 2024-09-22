a50fd49b81586c69e77cab4bbdd58fa66d18e646
Muhammad Saqib <sm4179515@gmail.com>
2024-09-22 14:53:44 +0500

    Add initial static user data

--- server.mjs
+++ server.mjs
@@ -3,3 +3,8 @@ import express from 'express';
 const PORT = 3000;
 const app = express();
 app.use(express.json());
+
+const users = [
+    { id: 1, name: 'khan', email: 'khan@gmail.com' },
+    { id: 2, name: 'jan', email: 'jan@gmail.com' },
+  ];
