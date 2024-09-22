65b172db6b24746f292b5e6bcfb3c93a5ff9620c
Muhammad Saqib <sm4179515@gmail.com>
2024-09-22 15:03:18 +0500

    some error done

--- server.mjs
+++ server.mjs
@@ -9,8 +9,7 @@ const users = [
     { id: 2, name: 'jan', email: 'jan@gmail.com' },
   ];
 
-  app.post('/api/users', (req, res) => {
-    const userId = users.length + 1;
+  app.post('/api/users', (req, re 1;
     const newUser = { id: userId, ...req.body };
     users.push(newUser);
     res.status(201).json({ message: 'Post Request - created a user', newUser });
