35559d06530f5693264cab6e62b07147e81dc45a
Muhammad Saqib <sm4179515@gmail.com>
2024-09-23 14:13:38 +0500

    Server Done

--- server.mjs
+++ server.mjs
@@ -1,55 +1,65 @@
-import express from 'express'; 
+import express from 'express';
 
 const PORT = 3000;
 const app = express();
 app.use(express.json());
 
 const users = [
-    { id: 1, name: 'khan', email: 'khan@gmail.com' },
-    { id: 2, name: 'jan', email: 'jan@gmail.com' },
-  ];
+  { id: 1, name: 'khan', email: 'khan@gmail.com' },
+  { id: 2, name: 'jan', email: 'jan@gmail.com' },
+];
 
-  app.post('/api/users', (req, re 1;
-    const newUser = { id: userId, ...req.body };
-    users.push(newUser);
-    res.status(201).json({ message: 'Post Request - created a user', newUser });
-  });
+// GET USERS
+app.get('/api/users', (req, res) => {
+  res.status(200).json({ message: 'Get Request - get all user', users });
+});
 
-  app.put('/api/users/:id', (req, res) => {
-    const userId = parseInt(req.params.id);
-    const userIndex = users.findIndex((user) => user.id === userId);
-  
-    if (userIndex !== -1) {
-      const updatedUser = { ...users[userIndex], ...req.body };
-      users[userIndex] = updatedUser;
-      res.status(200).json({
+// CREATE USER
+app.post('/api/users', (req, res) => {
+  const userId = users.length + 1;
+  const newUser = { userId, ...req.body };
+  users.push(newUser);
+  res.status(201).json({ message: 'Post Request - created a user', newUser });
+});
+
+// UPDATED USER
+app.put('/api/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  const userIndex = users.filter((user) => user.id === userId);
+  if (userIndex !== -1) {
+    const updatedUser = { ...users[userIndex], ...req.body };
+    users[userIndex] = updatedUser;
+    res
+      .status(200)
+      .json({
         message: `Update Request - The user with user id ${userId} is updated`,
-        updatedUser,
+        body: updatedUser,
       });
-    } else {
-      res.status(404).json({ message: `The user with user id ${userId} is not found` });
-    }
-  });
-  
-  app.delete('/api/users/:id', (req, res) => {
-    const userId = parseInt(req.params.id);
-    const userIndex = users.findIndex((user) => user.id === userId);
-  
-    if (userIndex !== -1) {
-      users.splice(userIndex, 1);
-      res.status(200).json({ message: `Delete Request - The user with user id ${userId} is deleted` });
-    } else {
-      res.status(404).json({ message: `The user with user id ${userId} is not found` });
-    }
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Server is listening on http://localhost:${PORT}`);
-  });
- 
-  
-  
+  } else {
+    res
+      .status(404)
+      .json({ message: `The user with user id ${userId} is not found` });
+  }
+});
 
-  app.get('/api/users', (req, res) => {
-    res.status(200).json({ message: 'Get Request - get all users', users });
-  });
+// DELETE USER
+app.delete('/api/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  const userIndex = users.filter((user) => user.id === userId);
+  if (userIndex !== -1) {
+    users.splice(userIndex, 1);
+    res
+      .status(200)
+      .json({
+        message: `Delete Request - The user with user id ${userId} is deleted`,
+      });
+  } else {
+    res
+      .status(404)
+      .json({ message: `The user with user id ${userId} is not found` });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`server is listening on http://localhost:3000`);
+});
